b82bd3057dcd77aa9dc33eacb27990f3e8760510
Dhirazcode <dhiraj@academiacollege.edu.np>
2024-08-26 23:56:48 +0545

    Signup

---
+++ Frontend/src/components/signup.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+
+const signup = () => {
+    const [input , setInput]= useState({
+        username:'',
+        email:'',
+        password:''
+    })
+    const  changeEventHandler =(e)=>{
+        console.log(input)
+        setInput({...input , [e.target.name]:e.target.value})
+    }
+
+    const signuphandler = async (e)=>{
+        e.preventDefault();
+    }
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+        <form onSubmit={signuphandler} className='flex flex-col gap-5 p-8 shadow-lg'>
+            <div className='my-4'>
+                <h1 className=' text-center font-bold text-xl'>LOGO</h1>
+                <p className='text-sm text-center'>Signup to to see photos and videos of your Friend</p>
+            </div>
+           <div>
+           <span className=' font-medium'>Username</span>
+            <Input 
+            type="text" 
+            name='username'
+            value={input.username}
+            onChange={changeEventHandler}
+            placeholder='Enter your name'
+             className=' focus-visible:ring-transparent  '>
+            </Input>
+           </div>
+           <div>
+           <span className=' font-medium'>Email</span>
+            <Input 
+            type="text"
+            name='email'
+            value={input.email}
+            onChange={changeEventHandler}
+             placeholder='Enter your email' 
+             className=' focus-visible:ring-transparent py-2'>
+            </Input>
+           </div>
+           <div>
+           <span className=' font-medium'>Password</span>
+            <Input 
+            type="text" 
+            name='password'
+            value={input.password}
+            onChange={changeEventHandler}
+            placeholder='Enter your password' 
+            className=' focus-visible:ring-transparent py-2'>
+            </Input>
+           </div>
+           <Button type='submit'>Signup</Button>
+        </form>
+    </div>
+  )
+}
+
+export default signup
